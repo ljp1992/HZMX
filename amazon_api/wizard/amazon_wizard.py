bd8e868545d2597c1bb2954699dc3879dc5a09a4
ljp1992 <1121403085>
2018-02-26 16:01:38 +0800

    delivery 1.2.1

--- amazon_api/wizard/amazon_wizard.py
+++ amazon_api/wizard/amazon_wizard.py
@@ -9,7 +9,8 @@ class AmazonWizard(models.TransientModel):
     _name = "amazon.wizard"
 
     # name = fields.Char()
-    logistics_company = fields.Char(string=u'物流公司')
+    logistics_company_code = fields.Char(string=u'物流公司代码')
+    logistics_company_name = fields.Char(string=u'物流公司名称')
     shipment_number = fields.Char(string=u'物流单号')
 
     delivery_time = fields.Datetime(string=u'发货时间', default=lambda self: datetime.datetime.now())
@@ -32,8 +33,7 @@ class AmazonWizard(models.TransientModel):
             message_info = ''
             for line in order.order_line:
                 message_id += 1
-                message_info += """
-                <Message>
+                message_info += """<Message>
                     <MessageID>%d</MessageID>
                     <OperationType>Update</OperationType>
                     <OrderFulfillment>
@@ -49,20 +49,18 @@ class AmazonWizard(models.TransientModel):
                             <Quantity>%s</Quantity>
                         </Item>
                     </OrderFulfillment>
-                </Message>""" % (message_id, AmazonOrderID, FulfillmentDate, self.logistics_company,
-                                 order.shipment_service_level_category, self.shipment_number, line.order_item_id,
-                                 line.product_uom_qty)
+                </Message>""" % (message_id, AmazonOrderID, FulfillmentDate,
+                                 self.logistics_company_name, order.shipment_service_level_category,
+                                 self.shipment_number, line.order_item_id, line.product_uom_qty)
             head = """<?xml version="1.0" encoding="utf-8"?>
-    <AmazonEnvelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="amzn-envelope.xsd">
-        <Header>
-            <DocumentVersion>1.01</DocumentVersion>
-                <MerchantIdentifier>%s</MerchantIdentifier>
-        </Header>
-        <MessageType>OrderFulfillment</MessageType>
-        %s
-    </AmazonEnvelope>""" % (seller.merchant_id_num, message_info)
-            print head
-            # return
+                <AmazonEnvelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="amzn-envelope.xsd">
+                    <Header>
+                        <DocumentVersion>1.01</DocumentVersion>
+                            <MerchantIdentifier>%s</MerchantIdentifier>
+                    </Header>
+                    <MessageType>OrderFulfillment</MessageType>
+                    %s
+                </AmazonEnvelope>""" % (seller.merchant_id_num, message_info)
             mws_obj = Feeds(access_key=str(seller.access_key), secret_key=str(seller.secret_key),
                             account_id=str(seller.merchant_id_num), region=shop.country_id.code, proxies={})
             try:
@@ -70,7 +68,6 @@ class AmazonWizard(models.TransientModel):
             except Exception, e:
                 raise UserError(str(e))
             FeedSubmissionId = result.parsed.get('FeedSubmissionInfo', {}).get('FeedSubmissionId', {}).get('value', '')
-            print FeedSubmissionId
             if FeedSubmissionId:
                 self.env['submission.history'].create({
                     'model': 'sale.order',
