a8e548298749e4d652f2a5c6a7c62d66dd0f0fce
ljp1992 <1121403085>
2018-03-02 07:39:15 +0800

    update currency rate

--- amazon_api/models/currency.py
+++ amazon_api/models/currency.py
@@ -8,5 +8,12 @@ class AmazonCurrency(models.Model):
     name = fields.Char(string=u'名称')
     symbol = fields.Char(string=u'符号')
 
-    rate = fields.Float(digits=(16,6), string=u'当前汇率')
+    # rate = fields.Float(digits=(16,6), string=u'当前汇率')
+    rate = fields.Float(compute='_compute_rate', digits=(16,6), store=False, string=u'当前汇率')
 
+    @api.multi
+    def _compute_rate(self):
+        for record in self:
+            currency = self.env['res.currency'].search([('name', '=', record.name)], limit=1)
+            if currency:
+                record.rate = currency.rate
--- amazon_api/test/cs.py
+++ amazon_api/test/cs.py
@@ -1,9 +1,26 @@
-# -*- coding: utf-8 -*-
+# -*- coding:utf-8 -*-
+import re
+from lxml import etree
+import requests
 
-a = [
-    {
-        'ListParticipations': {'Participation': [{'HasSellerSuspendedListings': {'value': 'No'}, 'SellerId': {'value': 'AU3KSTW4DW2XE'}, 'value': '\n        ', 'MarketplaceId': {'value': 'A1MQXOICRS2Z7M'}}, {'HasSellerSuspendedListings': {'value': 'No'}, 'SellerId': {'value': 'AU3KSTW4DW2XE'}, 'value': '\n        ', 'MarketplaceId': {'value': 'A2EUQ1WTGCTBG2'}}, {'HasSellerSuspendedListings': {'value': 'No'}, 'SellerId': {'value': 'AU3KSTW4DW2XE'}, 'value': '\n        ', 'MarketplaceId': {'value': 'A2ZV50J4W1RKNI'}}], 'value': '\n      '}, 'ListMarketplaces': {'Marketplace': [{'DefaultCountryCode': {'value': 'CA'}, 'Name': {'value': 'SI CA Prod Marketplace'}, 'DomainName': {'value': 'siprod.stores.amazon.ca'}, 'DefaultLanguageCode': {'value': 'en_CA'}, 'value': '\n        ', 'DefaultCurrencyCode': {'value': 'CAD'}, 'MarketplaceId': {'value': 'A1MQXOICRS2Z7M'}}, {'DefaultCountryCode': {'value': 'CA'}, 'Name': {'value': 'Amazon.ca'}, 'DomainName': {'value': 'www.amazon.ca'}, 'DefaultLanguageCode': {'value': 'en_CA'}, 'value': '\n        ', 'DefaultCurrencyCode': {'value': 'CAD'}, 'MarketplaceId': {'value': 'A2EUQ1WTGCTBG2'}}, {'DefaultCountryCode': {'value': 'US'}, 'Name': {'value': 'Non-Amazon'}, 'DomainName': {'value': 'sim1.stores.amazon.com'}, 'DefaultLanguageCode': {'value': 'en_US'}, 'value': '\n        ', 'DefaultCurrencyCode': {'value': 'USD'}, 'MarketplaceId': {'value': 'A2ZV50J4W1RKNI'}}], 'value': '\n      '}, 'value': '\n    '}]
+url = 'http://www.boc.cn/sourcedb/whpj/index.html'  # 网址
+html = requests.get(url).content.decode('utf8')  # 获取网页源码（中间涉及到编码问题,这是个大坑，你得自己摸索）
+print html
+# 方式一：正则匹配
+a = html.index(u'<td>新西兰元</td>')  # 取得“新西兰元”当前位置
+s = html[a:a + 300]  # 截取新西兰元汇率那部分内容（从a到a+300位置）
+result = re.findall('<td>(.*?)</td>', s)  # 正则获取
+print result
 
-for item in a:
-    for it in item.get('ListMarketplaces', {}).get('Marketplace', []):
-        print it
+# 方式二：lxml获取
+# result=etree.HTML(html).xpath('//table[@cellpadding="0"]/tr[18]/td/text()')
+
+#写入txt
+# with open('./汇率.txt', 'w+') as f:
+#     f.write(result[0] + '\n')
+#     f.write(u'现汇买入价：' + result[1] + '\n')
+#     f.write(u'现钞买入价：' + result[2] + '\n')
+#     f.write(u'现汇卖出价：' + result[3] + '\n')
+#     f.write(u'现钞卖出价：' + result[4] + '\n')
+#     f.write(u'中行折算价：' + result[5] + '\n')
+#     f.write(u'发布时间：' + result[6] + result[7] + '\n')
