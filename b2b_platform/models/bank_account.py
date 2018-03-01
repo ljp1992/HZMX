1b4ed66d38c4cd5fcfa515fa908ba9a61b41cfa4
ljp1992 <1121403085>
2018-03-02 04:21:15 +0800

    security

--- b2b_platform/models/bank_account.py
+++ b2b_platform/models/bank_account.py
@@ -13,7 +13,7 @@ class BankAccount(models.Model):
     bank_id = fields.Many2one('res.bank', domain=lambda self: self._bank_id_domain(), required=True, string=u'银行')
 
     merchant_id = fields.Many2one('res.users', default=lambda self: self.env.user.merchant_id or self.env.user,
-                                  string=u'商户')
+                                  string=u'户主')
 
     @api.model
     def _get_platform(self):
@@ -29,7 +29,27 @@ class BankAccount(models.Model):
         elif self.user_has_groups('b2b_platform.b2b_seller'):
             return [('merchant_id', '=', self.env.user.id)]
         elif self.user_has_groups('b2b_platform.b2b_manager'):
-            return [('merchant_id', '=', self.env.user.id)]
-        else:
-            return [('merchant_id', '=', self.env.user.id)]
+            return [('platform', '=', True)]
+
+    @api.model
+    def name_search(self, name, args=None, operator='ilike', limit=100):
+        args = args or []
+        if name:
+            args += [('name', operator, name)]
+        result = self.search(args, limit=limit)
+        return result.name_get()
+
+    @api.model
+    def search(self, args, offset=0, limit=None, order=None, count=False):
+        context = self.env.context or {}
+        if context.get('view_own_bank_account'):
+            if self.user_has_groups('b2b_platform.b2b_shop_operator'):
+                args += [('merchant_id', '=', self.env.user.merchant_id.id)]
+            elif self.user_has_groups('b2b_platform.b2b_seller'):
+                args += [('merchant_id', '=', self.env.user.id)]
+            elif self.user_has_groups('b2b_platform.b2b_manager'):
+                pass
+            else:
+                pass
+        return super(BankAccount, self).search(args, offset, limit, order, count=count)
 
--- b2b_platform/models/res_bank.py
+++ b2b_platform/models/res_bank.py
@@ -9,7 +9,7 @@ class ResBank(models.Model):
     platform = fields.Boolean(default=lambda self: self._get_platform(), help=u'标识该银行是否为平台银行')
 
     merchant_id = fields.Many2one('res.users', default=lambda self: self.env.user.merchant_id or self.env.user,
-                                  string=u'商户')
+                                  string=u'创建人')
 
     @api.model
     def _get_platform(self):
@@ -17,3 +17,25 @@ class ResBank(models.Model):
             return True
         else:
             return False
+
+    @api.model
+    def name_search(self, name, args=None, operator='ilike', limit=100):
+        args = args or []
+        if name:
+            args += [('name', operator, name)]
+        result = self.search(args, limit=limit)
+        return result.name_get()
+
+    @api.model
+    def search(self, args, offset=0, limit=None, order=None, count=False):
+        context = self.env.context or {}
+        if context.get('view_own_bank'):
+            if self.user_has_groups('b2b_platform.b2b_shop_operator'):
+                args += [('merchant_id', '=', self.env.user.merchant_id.id)]
+            elif self.user_has_groups('b2b_platform.b2b_seller'):
+                args += [('merchant_id', '=', self.env.user.id)]
+            elif self.user_has_groups('b2b_platform.b2b_manager'):
+                pass
+            else:
+                pass
+        return super(ResBank, self).search(args, offset, limit, order, count=count)
